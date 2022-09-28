9d19799d09a68e70032f674b770eaba85b8f6188
Reiji Terunuma <reiji.210@icloud.com>
2022-09-28 16:34:51 +0900

    add webcam program

--- Self_Solving_Rubik_Cube/main/main2.py
+++ Self_Solving_Rubik_Cube/main/main2.py
@@ -2,9 +2,11 @@ import random
 from shutil import move
 import time 
 from time import sleep
+import cv2
 from hardware import motors
 from algorithm import state
 from algorithm import search
+from algorithm import detect_color
 
 """main関数"""
 if __name__ == '__main__':
@@ -12,6 +14,11 @@ if __name__ == '__main__':
     scramble_length = 20
     random_scramble = state.Create_scramble(scramble_length)
     print("random_scramble = ", random_scramble)
+
+    # """Webcamで撮影した画像からstateを求める"""
+    # color_state = detect_color.Get_color_state()
+    # random_scramble = state.color2state(color_state)
+    # print("random_scramble = ", random_scramble)
 
     """モータ動作"""
     print('start scrambling')
