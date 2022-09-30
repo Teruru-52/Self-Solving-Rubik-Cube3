fd325dd5839f60eb0a4f4cb4458589d960ebf881
Reiji Terunuma <reiji.210@icloud.com>
2022-09-30 10:57:29 +0900

    add webcam4,6 process

--- Webcam/detect_color.py
+++ Webcam/detect_color.py
@@ -12,13 +12,20 @@ color_state = ColorState(
     [['',''], ['',''], ['',''], ['',''], ['',''], ['',''], ['',''], ['',''], ['',''], ['',''], ['',''], ['','']]
 )
 
+# webcam0.jpg
 correct_state = ColorState(
     [['','',''], ['','',''], ['','O','B'], ['','',''], ['','',''], ['O','',''], ['B','Y','R'], ['','','']],
     [['',''], ['',''], ['B','O'], ['',''], ['',''], ['',''], ['',''], ['',''], ['',''], ['O','G'], ['Y','B'], ['','']]
 )
 
+# webcam4.jpg
+correct_state = ColorState(
+    [['Y','',''], ['Y','R','B'], ['','',''], ['','',''], ['','',''], ['','G','R'], ['','',''], ['','','']],
+    [['',''], ['R','G'], ['',''], ['',''], ['W','R'], ['Y','B'], ['',''], ['',''], ['',''], ['',''], ['',''], ['','']]
+)
+
 def Set_color_state(color, camera_no, index):
-    if camera_no == 1:
+    if camera_no == 0:
         if index == 0:
             color_state.cc[2][2] = color
         elif index == 1:
@@ -70,18 +77,81 @@ def Set_color_state(color, camera_no, index):
         elif index == 11:
             color_state.cc[7][0] = color
 
+    elif camera_no == 4:
+        if index == 0:
+            color_state.cc[1][0] = color
+        elif index == 1:
+            color_state.cc[1][1] = color
+        elif index == 2:
+            color_state.cc[1][2] = color
+        elif index == 3:
+            color_state.ec[5][0] = color
+        elif index == 4:
+            color_state.ec[5][1] = color
+        elif index == 5:
+            color_state.ec[4][0] = color
+        elif index == 6:
+            color_state.ec[4][1] = color
+        elif index == 7:
+            color_state.cc[0][0] = color
+        elif index == 8:
+            color_state.ec[1][0] = color
+        elif index == 9:
+            color_state.ec[1][1] = color
+        elif index == 10:
+            color_state.cc[5][2] = color
+        elif index == 11:
+            color_state.cc[5][1] = color
+
+    elif camera_no == 6:
+        if index == 0:
+            color_state.cc[3][0] = color
+        elif index == 1:
+            color_state.cc[3][1] = color
+        elif index == 2:
+            color_state.cc[3][2] = color
+        elif index == 3:
+            color_state.ec[7][0] = color
+        elif index == 4:
+            color_state.ec[7][1] = color
+        elif index == 5:
+            color_state.ec[6][0] = color
+        elif index == 6:
+            color_state.ec[6][1] = color
+        elif index == 7:
+            color_state.cc[0][0] = color
+        elif index == 8:
+            color_state.ec[3][0] = color
+        elif index == 9:
+            color_state.ec[3][1] = color
+        elif index == 10:
+            color_state.cc[7][2] = color
+        elif index == 11:
+            color_state.cc[7][1] = color
+
 k_nn = knn.K_NN(k = 1) 
-training_red = [[5.64, 164.77, 155.87]]
-training_blue = [[107.05, 144.05, 220.48], [104.81, 179.99, 89.31], [105.99, 156.25, 188.84], [106.970625, 145.05375, 231.868125]]
-training_green = [[55.04, 78.68, 151.31]]
-training_orenge = [[7.94, 136.29, 254.84], [5.99375, 150.715, 254.775], [9.1575, 163.935, 168.3375], [11.07, 169.22, 155.98]]
-training_yellow = [[35.15, 75.04, 254.41], [34.06, 63.27, 189.34]]
-training_data = training_red + training_blue + training_green + training_orenge + training_yellow
-label = ['R' for i in range(len(training_red))] + \
-        ['B' for i in range(len(training_blue))] + \
-        ['G' for i in range(len(training_green))] + \
-        ['O' for i in range(len(training_orenge))] + \
-        ['Y' for i in range(len(training_yellow))]
+# webcam0
+# train_red = [[5.64, 164.77, 155.87]]
+# train_blue = [[107.05, 144.05, 220.48], [104.81, 179.99, 89.31], [105.99, 156.25, 188.84], [106.970625, 145.05375, 231.868125]]
+# train_green = [[55.04, 78.68, 151.31]]
+# train_orenge = [[7.94, 136.29, 254.84], [5.99375, 150.715, 254.775], [9.1575, 163.935, 168.3375], [11.07, 169.22, 155.98]]
+# train_yellow = [[35.15, 75.04, 254.41], [34.06, 63.27, 189.34]]
+# train_white = []
+# webcam4
+train_red = [[3.93, 246.88, 133.07], [4.04, 246.06, 122.3], [4.23, 252.13, 93.78], [12.46, 252.04, 59.92]]
+train_blue = [[107.05, 243.13, 197.07], [104.29, 248.01, 79.38]]
+train_green = [[65.37, 218.34, 223.41], [65.18, 216.24, 206.59]]
+train_orenge = []
+train_yellow = [[32.02, 3.66, 255.0], [36.19, 125.51, 179.8], [32.97, 43.0, 238.78]]
+train_white = [[46.3, 16.95, 218.53]]
+
+train_data = train_red + train_blue + train_green + train_orenge + train_yellow + train_white
+label = ['R' for i in range(len(train_red))] + \
+        ['B' for i in range(len(train_blue))] + \
+        ['G' for i in range(len(train_green))] + \
+        ['O' for i in range(len(train_orenge))] + \
+        ['Y' for i in range(len(train_yellow))] + \
+        ['W' for i in range(len(train_white))]
 
 class Camera:
     def __init__(self, xy, cam_no):
@@ -98,26 +168,23 @@ class Camera:
         v = imgBoxHsv.T[2].flatten().mean()
 
         # HSV平均値を出力
-        # print("Hue: %.2f" % (h))
-        # print("Saturation: %.2f" % (s))
-        # print("Value: %.2f" % (v))
         hsv = [h, s, v]
         # print(list(map(round, hsv, [2]*len(hsv))))
 
         return [h, s, v]
 
     def Identificate_color(self, hsv):
-        k_nn.fit(training_data, label)
+        k_nn.fit(train_data, label)
         color = k_nn.predict([hsv])
 
         # print(color)
         return color
 
     def camera2color_state(self):
-        img = cv2.imread('picture/001.jpg')
+        img = cv2.imread(f'picture/webcam{self.cam_no}.jpg')
 
-        lateral = 40        
-        for i in range(12):
+        lateral = 30        
+        for i in range(len(self.xy)):
             # 対象範囲を切り出し
             imgBox = img[self.xy[i][1]: self.xy[i][1]+lateral, self.xy[i][0]: self.xy[i][0]+lateral]
             # HSVの平均値を取得
@@ -128,27 +195,32 @@ class Camera:
             Set_color_state(color, self.cam_no, i)
 
         # draw rectangle
-        # cv2.rectangle(img, (self.xy[6][0], self.xy[6][1]), (self.xy[6][0]+lateral, self.xy[][1]+lateral), (255, 0, 0), thickness=4)
-        for i in range(12):
-            cv2.rectangle(img, (self.xy[i][0], self.xy[i][1]), (self.xy[i][0]+lateral, self.xy[i][1]+lateral), (255, 0, 0), thickness=4)
+        cv2.rectangle(img, (self.xy[8][0], self.xy[8][1]), (self.xy[8][0]+lateral, self.xy[8][1]+lateral), (255, 0, 0), thickness=4)
+        # for i in range(len(self.xy)):
+        #     cv2.rectangle(img, (self.xy[i][0], self.xy[i][1]), (self.xy[i][0]+lateral, self.xy[i][1]+lateral), (255, 0, 0), thickness=4)
 
         print("cc = ", color_state.cc)
         print("ccc = ", correct_state.cc)
         print("ec = ", color_state.ec)
         print("cec = ", correct_state.ec)
-        cv2.imwrite('picture/result.jpg', img)
+        cv2.imwrite(f'picture/result{self.cam_no}.jpg', img)
 
 def Get_color_state():
     return color_state
 
-xy_camera1 = [[460, 50], [590, 50], [460, 190], [590, 190], [460, 330], [590, 330], [525, 450],
+xy_camera0 = [[460, 50], [590, 50], [460, 190], [590, 190], [460, 330], [590, 330], [525, 450],
               [360, 380], [400, 490], [690, 380], [650, 490], [710, 515]]
+
+xy_camera4 = [[260, 170], [300, 230], [220, 230], [180, 130], [140, 190], [340, 130], [380, 190],
+              [400, 110], [300, 340], [220, 340], [300, 430], [220, 430]]
 
 #main
 if __name__ == '__main__':
     try:
-        camera1 = Camera(xy_camera1, 1)
-        camera1.camera2color_state()
+        camera0 = Camera(xy_camera0, 0)
+        camera0.camera2color_state()
+        # camera4 = Camera(xy_camera4, 4)
+        # camera4.camera2color_state()
 
     except KeyboardInterrupt:
         pass
