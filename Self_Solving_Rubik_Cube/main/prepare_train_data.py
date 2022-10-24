303f1918cc5dddd93f24c83f8df7df9506f31360
Reiji Terunuma <reiji.210@icloud.com>
2022-10-25 04:26:02 +0900

    学習データを増やした

--- Self_Solving_Rubik_Cube/main/prepare_train_data.py
+++ Self_Solving_Rubik_Cube/main/prepare_train_data.py
@@ -11,7 +11,19 @@ if __name__ == '__main__':
         detect_color.Train_data('train', 1)
         motor.Solve("R L")
         detect_color.Train_data('train', 2)
-        motor.Solve("R' L'")
+        motor.Solve("R L")
+        detect_color.Train_data('train', 3)
+        motor.Solve("R L")
+        detect_color.Train_data('train', 4)
+        motor.Solve("R L")
+        
+        motor.Solve("F B")
+        detect_color.Train_data('train', 5)
+        motor.Solve("F B")
+        detect_color.Train_data('train', 6)
+        motor.Solve("F B")
+        detect_color.Train_data('train', 7)
+        motor.Solve("F B")
 
         detect_color.Print_train_data()
 
