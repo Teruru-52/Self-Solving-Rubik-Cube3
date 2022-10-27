4dc007993e322d41222d87ccfcce99b297bcb7c2
Reiji Terunuma <reiji.210@icloud.com>
2022-10-28 02:26:46 +0900

    add random scramble solving

--- Self_Solving_Rubik_Cube/main/main_random_solve.py
+++ Self_Solving_Rubik_Cube/main/main_random_solve.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
 
     """完成状態の設定"""
     mode = 'normal'
-    # 2回繰り返すと元に戻る
     # mode = 'checker'
     # mode = 'checker2'
     # mode = 'H'
@@ -33,19 +32,24 @@ if __name__ == '__main__':
     # mode = 'angel_fish'
     # mode = 'convex'
     # mode = 'ring'
-
+    
     """scrambled_stateの生成"""
     """1.1 6面同色状態から模様の状態にする"""
     # scrambled_state = state.Set_next_state('normal', mode)
 
     """1.2 模様の状態から6面同色状態に戻す"""
-    scrambled_state = state.Set_next_state(mode, 'normal')
+    # scrambled_state = state.Set_next_state(mode, 'normal')
 
-    """2. Webcamで画像を撮影し, stateを求める場合"""
-    # detect_color.Take_pictures()
-    # color_state = detect_color.Get_color_state()
-    # scrambled_state = state.Set_solved_state(mode, color_state)
-    # print("scrambled_state: ", scrambled_state)
+    """scrambled_stateの生成"""
+    """2. 完成状態からrandomに回してstateを求める"""
+    scramble_length = 20
+    random_scramble = state.Create_scramble(scramble_length)
+    print("random_scramble: ", random_scramble)
+    print('start scrambling')
+    motor.Solve(random_scramble)
+    scrambled_state = state.scamble2state(random_scramble)
+    
+    sleep(1.0)
 
     """Phase2探索プログラムの動作確認"""
     search2 = search.Search2(scrambled_state)
